3412e5034929575d71f984b52c0c500ef8e6e3c2
서동경 <ehhdrud@icloud.com>
2022-08-31 14:14:57 +0900

    220831

---
+++ 빵 부스러기/getter,setter.js
@@ -0,0 +1,32 @@
+/**
+ * 프로퍼티 접근자: 본질은 함수인데 외부 코드에서는 함수가 아닌 일반적인 프로퍼티 처럼 보임
+ * getter:획득자, setter:설정자
+ * 이를 실제 프로퍼티 값을 감싸는 래퍼(Wrapper)처럼 사용한다면, 프로퍼티 값을 통제하기에 유용함
+ */
+const person = {
+  fname: "seo",
+  lname: "dong-kyeong",
+
+  //getter 메서드
+  get firstName() {
+    return this.fname.toUpperCase();
+  },
+
+  //setter 메서드
+  set firstName(newFirstName) {
+    if (newFirstName !== "string") {
+      this.fname = "undifined first name";
+
+      return;
+    }
+    this.fname = newFirstName;
+  },
+
+  //getter 메서드2
+  get fullName() {
+    return this.fname + ` ` + this.lname;
+  },
+};
+
+console.log(person.firstName);
+console.log(person.fullName);
